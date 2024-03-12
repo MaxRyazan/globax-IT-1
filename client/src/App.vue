ba062979a9517f03ee16eb9ea05afe878bef0344
MaxRyazan <no4ta87@mail.ru>
2024-03-12 22:54:00 +0300

    add input

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,6 +5,7 @@ import {TUser} from "../types";
 
 const USERS_URL = 'http://localhost:3000'
 const users: Ref<TUser[]> = ref([]);
+const searchParam = ref('')
 
 onMounted(async () => {
     const result: Response = await fetch(USERS_URL);
@@ -13,11 +14,19 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div class="wrapper">
-        <user-card v-for="user in users" :key="user.email" :user/>
+    <div class="container">
+        <input class="search" type="text" v-model="searchParam">
+        <div class="wrapper">
+            <user-card v-for="user in users" :key="user.email" :user/>
+        </div>
     </div>
 </template>
 <style scoped lang="scss">
+.container{
+  max-width: 1121px;
+  margin: 0 auto;
+  padding: 40px 0;
+}
 .wrapper {
   max-width: 1121px;
   margin: 0 auto;
@@ -25,5 +34,13 @@ onMounted(async () => {
   flex-wrap: wrap;
   gap: 24px;
   padding: 20px 0;
+}
+.search {
+  width: 1121px;
+  border: 1px solid rgba(212, 222, 254, 1);
+  height: 48px;
+  border-radius: 24px;
+  font-size: 22px;
+  padding: 0 20px 0 50px;
 }
 </style>
